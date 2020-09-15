ad684141c6b512e7c41520efe01805c640b96259
bogukom <bogusz5752@gmail.com>
2020-09-16 01:56:50 +0200

    HerokuApp tests

--- src/test/java/BaseDriverSettings.java
+++ src/test/java/BaseDriverSettings.java
@@ -15,20 +15,29 @@ public abstract class BaseDriverSettings {
 
         if (operatingSystem.contains("win")) {
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bogu\\Downloads\\chromedriver.exe");
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1200x600");
+            driver = new ChromeDriver(options);
         }
-        else if (operatingSystem.contains("lin") || operatingSystem.contains("nux")) {
+        else if (operatingSystem.contains("lin")) {
             System.setProperty("webdriver.chrome.driver", "\\home\\pi\\Downloads\\chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1200x600");
+            options.setBinary("\\usr\\lib\\chromium-browser");
+            driver = new ChromeDriver(options);
         }
 //        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bogu\\Downloads\\chromedriver.exe");
 //        System.out.println(System.getProperty("os.name"));
 //        if(System.getProperty("os.name").toLowerCase().contains("win"))
 //            chromedriverPath+=".exe";
 //        System.setProperty("webdriver.chrome.driver",chromedriverPath);
-        driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--window-size=1200x600");
-        driver = new ChromeDriver(options);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("--window-size=1200x600");
+//        driver = new ChromeDriver(options);
         initialize();
     }
 
